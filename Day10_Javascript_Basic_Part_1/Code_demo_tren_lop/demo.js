1d82c17380699e1933bd1f3b832be06c066b1065
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2021-09-20 21:37:34 +0700

    update

---
+++ Day10_Javascript_Basic_Part_1/Code_demo_tren_lop/demo.js
@@ -0,0 +1,6 @@
+// demo.js, tạo cùng cấp với file js_basic_1.html
+alert('File demo.js chạy khi trang đc tải');
+alert('File demo.js chạy khi trang đc tải');
+alert('File demo.js chạy khi trang đc tải');
+alert('File demo.js chạy khi trang đc tải');
+alert('File demo.js chạy khi trang đc tải');
