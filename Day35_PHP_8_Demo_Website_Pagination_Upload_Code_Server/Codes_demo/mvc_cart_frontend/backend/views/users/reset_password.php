b8fcb33f2ff8b33e2806475a70b4c818be363979
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2022-03-28 12:44:07 +0700

    update

---
+++ Day35_PHP_8_Demo_Website_Pagination_Upload_Code_Server/Codes_demo/mvc_cart_frontend/backend/views/users/reset_password.php
@@ -0,0 +1,9 @@
+<!--views/users/reset_password.php-->
+
+<form action="" method="post" class="container">
+    <h1>Form reset password</h1>
+    <label for="username">Nhập email cần reset mật khẩu</label>
+    <input type="email" name="username" value="" class="form-control" id="username" />
+    <br />
+    <input type="submit" name="submit" value="Lấy lại mật khẩu" class="btn btn-success" />
+</form>
